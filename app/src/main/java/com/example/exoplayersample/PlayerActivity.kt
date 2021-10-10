2361a8031a71c686255affc23b8a7f423270affe
mrky125 <4ex21ia@gmail.com>
2021-10-11 07:22:29 +0900

    support screen orientaion changing
    https://developer.android.com/guide/topics/resources/runtime-changes?hl=ja

--- app/src/main/java/com/example/exoplayersample/PlayerActivity.kt
+++ app/src/main/java/com/example/exoplayersample/PlayerActivity.kt
@@ -1,6 +1,8 @@
 package com.example.exoplayersample
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.exoplayersample.databinding.ActivityPlayerBinding
 
@@ -13,5 +15,16 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
+            supportActionBar?.hide()
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
+            supportActionBar?.show()
+        }
     }
 }
