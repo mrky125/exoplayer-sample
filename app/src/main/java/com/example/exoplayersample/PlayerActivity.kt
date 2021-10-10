ce873a62244bbd2620fc4a321e6c00164c6497cf
mrky125 <4ex21ia@gmail.com>
2021-10-11 07:39:20 +0900

    changed status bar visibility when orientation changed

--- app/src/main/java/com/example/exoplayersample/PlayerActivity.kt
+++ app/src/main/java/com/example/exoplayersample/PlayerActivity.kt
@@ -1,8 +1,11 @@
 package com.example.exoplayersample
 
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.exoplayersample.databinding.ActivityPlayerBinding
 
@@ -17,14 +20,17 @@ class PlayerActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
             supportActionBar?.hide()
+            binding.root.windowInsetsController?.hide(WindowInsets.Type.statusBars())
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
             supportActionBar?.show()
+            binding.root.windowInsetsController?.show(WindowInsets.Type.statusBars())
         }
     }
 }
